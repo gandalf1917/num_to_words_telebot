16579b4eff8d15967f1e317d81a4c275dd55ba35
Fedor_Sokolov <fe.sokolov19@gmail.com>
2023-10-18 21:39:18 +0300

    release v1

---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+from function import number_to_words
+
+# Создание экземпляра бота с указанием токена
+bot = telebot.TeleBot("6324446132:AAH2b36LRIOYyrXlj5OH_vN2uGNme_SIBYs")
+
+# Обработчик команды start
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(message, "Привет! Я бот, готовый принять твои сообщения.")
+
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    text = message.text
+
+    try:
+        # Преобразование числа в слова с помощью функции number_to_words
+        result = number_to_words(int(text))
+        bot.reply_to(message, result)
+    except ValueError:
+        bot.reply_to(message, "Ошибка! Введите число.")
+
+# Запуск бота
+bot.polling()
+
